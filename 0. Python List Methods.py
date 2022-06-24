031ea37183054008fe63de8d21d8e8c67992472a
ondersevik <osevik75@gmail.com>
2022-06-24 12:10:23 +0300

    Tamamlandı.

---
+++ 0. Python List Methods.py
@@ -0,0 +1,9 @@
+# python list methods :
+
+list = [1,2,3,4,5,6]
+print(list)
+list.append(6)   # [1, 2, 3, 4, 5, 6, 6]
+# list.clear()     # []
+# list.copy()
+print(list.count(6)) # 2 ( 2 tane 6 var)
+print(list.extend("6"))
